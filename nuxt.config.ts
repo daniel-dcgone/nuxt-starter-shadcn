89719376e5b4349e86be2b11663489247bafc64f
Daniel_Pickering <danielm.pickering@gmail.com>
2026-02-09 01:30:18 -0800

    base

---
+++ nuxt.config.ts
@@ -0,0 +1,43 @@
+import tailwindcss from "@tailwindcss/vite"
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+
+  modules: [
+    "shadcn-nuxt",
+    "@vueuse/nuxt",
+    "@nuxt/eslint",
+    "@nuxt/scripts",
+  ],
+  devtools: { enabled: true },
+  css: ["~/assets/css/tailwind.css"],
+  compatibilityDate: "2025-07-15",
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        quotes: "double",
+      },
+    },
+  },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: "@/components/ui",
+  },
+})
